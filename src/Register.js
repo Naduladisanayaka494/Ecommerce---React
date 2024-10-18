d28bef9f6db1d83316f56c8adbb506d75e711782
Naduladisanayaka494 <it20247140@my.sliit.lk>
2024-10-18 13:04:01 +0530

    added the register api and naviagted to add product

--- src/Register.js
+++ src/Register.js
@@ -1,12 +1,26 @@
 
-import React,{useState} from "react"
+import React, { useState } from "react"
+import { useNavigate } from 'react-router-dom';
 function Register() {
     const [name, SetName] = useState("")
     const [password, SetPassword] = useState("")
     const [email, SetEmail] = useState("")
+     const navigate = useNavigate();
     
-    function signup() {
-        console.log(name,password,email)
+    async function signup() {
+        let item={name,password,email}
+        console.warn(item)
+       let result= await fetch(" http://127.0.0.1:8000/api/register", {
+            method: 'POST',
+            body: JSON.stringify(item),
+            headers: {
+                "Content-Type": 'application/json',
+                "Accept":'application/json'
+            }
+       })
+      result = await result.json()
+        localStorage.setItem("user-info", JSON.stringify(result))
+        navigate("/add");
     }
     return (
         <div className="col-sm-6 offset-sm-3">
